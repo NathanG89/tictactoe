f044cf4e964b697d7937c8e057764aedcdbb11d1
Nathan Gerstmann <gnathan2152@gmail.com>
2015-04-25 22:36:34 -0700

    Button Class Changes 2
    Attempted to create the start menu with single and multi player buttons
    that allow the player to choose if they are playing alone or with others
    before the game starts.  The buttons display properly, but the function
    of the buttons still needs work, as the game crashes upon clicking on a
    button.
    
    Made changes to the gameexit method to allow an array of tuples
    containing the text and the delay time before the next message in the
    list displays.

--- graphics.py
+++ graphics.py
@@ -46,16 +46,20 @@ class Tile(Square):
 def create_graphic_board(size=SIZE):
     return Grid([Column([Tile(DISPLAY_SURF, x,y) for y in range(size)]) for x in range(size)])
 
+def redraw_board():
+    for tile in board: #loop that redraws the current state of the board to clear out previous messages
+        tile.draw()
+
 """takes a required argument 's' containing a string, and an optional 
 keyword argument 'coords' containing a dictionary of keywords accepted by the 
 pygame Rect function"""
 
 def message(s, coords={"left": 0,"top": 0}):
-    for tile in board: #loop that redraws the current state of the board to clear out previous messages
-        tile.draw()
+    #for tile in board: #loop that redraws the current state of the board to clear out previous messages
+        #tile.draw()
     msg = FONTOBJ.render(s, True, white, black)
     DISPLAY_SURF.blit(msg, msg.get_rect(**coords))
-    pygame.display.update() #updates the display
+    #pygame.display.update() #updates the display
     
 """Button class.  Initializes each instance with text passed in the argument
 'text' and the coordinates from the passed keyword argument 'coords'.
@@ -64,24 +68,28 @@ set to the center of the screen.  A function is also passed to the 'action'
 argument that the button class uses to run a function when clicked"""
 
 """The 'coords' keyword accepts a dictionary of keywords used by the pygame
-Rect function, which is then passed as a set of keywords in and of itself."""    
+Rect function, which is then passed as a set of keywords in and of itself.
+
+~Nate"""    
     
 class Button(object):
-    def __init__(self, text, action=None, coords={'centerx': 0, 'centery': 0}):
+    def __init__(self, args, text="button", action=None, coords={'centerx': 0, 'centery': 0}):
         self.text = text
         self.coords = coords
         self.action = action
+        self.args = args
         if action is None:
             self.button = FONTOBJ.render(self.text, True, black, gray)
         else:
             self.button = FONTOBJ.render(self.text, True, black, white)
     
-    def set_action(self, action=None):
+    def set_action(self, action=None, args):
         if action is None:
             self.button = FONTOBJ.render(self.text, True, black, gray)
         else:
             self.button = FONTOBJ.render(self.text, True, black, white)
         self.action = action
+        self.args = args
     
     def get_action(self):
         return self.action
@@ -99,52 +107,89 @@ class Button(object):
         return self.coords
         
     def draw(self):
+        #for tile in board: #loop that redraws the current state of the board to clear out previous messages
+            #tile.draw()
         DISPLAY_SURF.blit(self.button, self.button.get_rect(**self.coords))
-        pygame.display.update()
+        #pygame.display.update()
         
     def is_clicked(self): #checks to see if the button was clicked
         x,y = pygame.mouse.get_pos()
         return self.button.get_rect().collidepoint(x,y)
     
     def run(self):
-        self.action
+        self.action(*self.args)
+        
+"""method that renders messages to the display surface before closing the program.
 
-def gameexit(s, delay):
+messages are passed to the method as an array of tuples, where the first 
+item in the tuple is the string message and the second is the amount of delay
+set before the next message can be rendered.
+
+the coords keyword sets where the messages will be displayed in relation to 
+the display surface.  At this time I have only implemented passing 1 set of 
+coordinates, so all the messages will display in one location.  the closing
+message is set to displayin the center by default.
+
+~Updates By: Nate
+~Original Author: James"""
+
+def gameexit(info, coords={"left":0,"right":0}):
     cd = 3 #holds the count down timer value
-    message(s) #prints message from the argument variable to the display
-    sleep(delay)
-    s = 'Closing game...'
-    message(s, coords={"centerx": WIDTH/2, "centery": HEIGHT/2}) #prints message to the display
-    while cd != 0: #while loop counts down to program closing
-        message(s + str(cd), coords={"centerx": WIDTH/2, "centery": HEIGHT/2})
-        sleep(1)
+    for text, delay in info: #while loop counts down to program closing
+        redraw_board()
+        message(text, coords) #prints message from the argument variable to the display
+        pygame.display.update()
+        sleep(delay)
+    while cd != 0:
+        redraw_board()
+        message("Closing game in" + str(cd), coords={"centerx": WIDTH/2, "centery": HEIGHT/2})
+        pygame.display.update()
         cd -= 1; #count down variable decreased until while loop condition is met
+        sleep(1)
     pygame.quit() #close pygame modules
     sysexit() #closes program
+    
+def mode():
+    mode = None
+    modeButtons = {'single':Button("Single Player",action=redraw_board,coords={'centerx':WIDTH/2,'centery':(HEIGHT/2)+30}), 
+           'multi':Button("Multiplayer",action=redraw_board,coords={'centerx':WIDTH/2,'centery':(HEIGHT/2)-30})}
+    DISPLAY_SURF.fill(black)
+    redraw_board()
+    modeButtons['single'].draw()
+    modeButtons['multi'].draw()
+    pygame.display.flip()
+    while mode == None:
+        for button in modeButtons:
+            if modeButtons[button].is_clicked():
+                mode = button
+    return mode        
 
-buttons = {'single':Button("Single Player",coords={'centerx':WIDTH/2,'centery':(HEIGHT/2)+30}), 
-           'multi':Button("Multiplayer",coords={'centerx':WIDTH/2,'centery':(HEIGHT/2)-30})}
-buttons['single'].draw()
-buttons['multi'].draw()
+buttons = {}
 board = create_graphic_board(SIZE)
 clock = pygame.time.Clock()
 game = Game(players)
+mode = mode()
+text = 'Currently playing in ' + mode  + ' player mode'
+message(text, coords)
+sleep(4)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            gameexit('Closing game...', 1)
+            gameexit([('Closing game...', 1)], coords={"centerx": WIDTH/2, "centery": HEIGHT/2})
         if event.type == pygame.MOUSEBUTTONUP:
             for tile in board:
                 if tile.is_clicked():
                     if tile.toggle(game.players[game.current_turn].symbol):
                         game.next_player_turn()
                     if board.has_three_in_a_row():
-                        gameexit('game over, player %d won!' % game.current_turn, 4)
+                        gameexit([('game over, player %d won!' % game.current_turn, 4)])
             for button in buttons:
                 if buttons[button].is_clicked():
                     buttons[button].run()
     DISPLAY_SURF.fill(black)
+    redraw_board()
     message("Player %s, it's your turn" % game.current_turn)
+    #pygame.display.update()
     pygame.display.flip()
     clock.tick(60)
