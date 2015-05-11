ef06b027bf7c55fb7cdd016bff2e68da07c6d1a1
Nathan Gerstmann <guildwarrior159@hotmail.com>
2015-05-11 15:19:43 -0700

    New Unit Test: Intro Menu
    Created test for the intro menu.  Creating framework for tests to run.

---
+++ tests/test_intromenu.py
@@ -0,0 +1,15 @@
+from nose.tools import *
+from menu import *
+from graphics import *
+
+def test_init():
+    pass
+
+def test_btnSingleIntro():
+    pass
+
+def test_btnMultiIntro(): 
+    pass
+
+def test_btnQuitIntro():
+    pass
